a4b71e3dc8d0c6081976cd48ea1b50b9742e2e46
deme7trius <31722521+deme7trius@users.noreply.github.com>
2017-09-07 09:59:17 +0300

    TEST

--- functions.js
+++ functions.js
@@ -78,7 +78,7 @@ var x = setInterval(function() {
     
     // Output the result in an element with id="demo"
     document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+    + minutes + "m " + seconds + "s " + "UNTIL NEXT MATCH";
     
     // If the count down is over, write some text 
     if (distance < 0) {
