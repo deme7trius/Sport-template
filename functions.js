7adfb28027bd9b08e1946b620b0f374c9a86f676
deme7trius <31722521+deme7trius@users.noreply.github.com>
2017-11-17 16:17:29 +0200

    dasd

--- functions.js
+++ functions.js
@@ -59,7 +59,7 @@ $('#b').click(function(e) {
 // end function
       //countdown
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 22, 2017 15:00:00").getTime();
+var countDownDate = new Date("Dec 03, 2017 15:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
